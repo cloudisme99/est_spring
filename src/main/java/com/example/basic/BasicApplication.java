96ad6870591f39171e76f3bcf6001e0cd07e680b
HEO <cloudisme99@gmail.com>
2024-07-09 17:46:19 +0900

    day240709_구성편집_변수설정

---
+++ src/main/java/com/example/basic/BasicApplication.java
@@ -0,0 +1,27 @@
+package com.example.basic;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication(scanBasePackages = "com.example.basic.day240709")
+//@ComponentScan(basePackages = "com.example") 	// 위랑 같은 의미
+public class BasicApplication {
+
+	@Value("${server.port}")
+	private int port;
+
+	@Value("${spring.application.name}")
+	private String appName;
+
+	@PostConstruct
+	public void printConfig() {
+		System.out.println("포트번호: " + port);
+		System.out.println("애플리케이션 이름: " + appName);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApplication.class, args);
+	}
+}
