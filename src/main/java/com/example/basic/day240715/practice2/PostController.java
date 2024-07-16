8964ef51691401ba8e5c034bdb140c8eb1b9671d
HEO <cloudisme99@gmail.com>
2024-07-16 23:40:37 +0900

    day240716

--- src/main/java/com/example/basic/day240715/practice2/PostController.java
+++ src/main/java/com/example/basic/day240715/practice2/PostController.java
@@ -49,4 +49,34 @@ public class PostController {
 		model.addAttribute("post", post);
 		return "post/detail";
 	}
+
+	@PostMapping("/{id}/delete")
+	public String delete(@PathVariable("id") Long id){
+		posts.removeIf(post -> post.getId()==id);
+		return "redirect:/posts";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String editForm(@PathVariable("id") Long id, Model model) {
+		Post post = posts.stream()
+			.filter(p -> p.getId() == id)
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException());
+
+		model.addAttribute("post", post);
+		return "post/edit";
+	}
+
+	@PostMapping("/{id}/edit")
+	public String edit(@PathVariable("id") Long id, @ModelAttribute Post updatePost) {
+		Post post = posts.stream()
+			.filter(p -> p.getId() == id)
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException());
+		post.setTitle(updatePost.getTitle()); // set메서드로 업데이트 객체의 title 값으로 변경
+		post.setContent(updatePost.getContent()); // set 메서드로 엡데이터ㅡ 객체의 content 값으로 변경
+		return "redirect:/posts/{id}";
+	}
+
+
 }
