5f55da98835a90186aa84186d94e6b7b04659040
HEO <cloudisme99@gmail.com>
2024-07-18 23:35:40 +0900

    day240718 add

--- src/main/java/com/example/basic/day240717_restapi/BoardPostController.java
+++ src/main/java/com/example/basic/day240717_restapi/BoardPostController.java
@@ -1,8 +1,14 @@
 package com.example.basic.day240717_restapi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +24,50 @@ public class BoardPostController {
 		this.boardPostService = boardPostService;
 	}
 
+	// 게시글 생성
 	@PostMapping
 	public ResponseEntity<BoardPostDto> createBoardPost(@RequestBody BoardPostDto boardPostDto) {
 		BoardPostDto createdBoardPostDto = boardPostService.createBoardPost(boardPostDto);
 		return ResponseEntity.ok(createdBoardPostDto);
+	}
+
+	// 게시글 조회
+	@GetMapping
+	public ResponseEntity<List<BoardPostDto>> getAllBoardPosts() {
+		List<BoardPostDto> boardPostDtos = boardPostService
+			.getAllBoardPosts();
+		return ResponseEntity.ok(boardPostDtos);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<BoardPostDto> getBoardPostById(@PathVariable("id") Long id){
+		BoardPostDto boardPostDto = boardPostService.getBoardPostDtoById(id);
+		return ResponseEntity.ok(boardPostDto);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteBoardPost(@PathVariable("id") Long id){
+		boardPostService.deleteBoardPost(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<BoardPostDto> updateBoardPost(@PathVariable("id") Long id, @RequestBody
+	BoardPostDto updateBoardPostDto){
+		BoardPostDto updatedBoardPostDto = boardPostService.updateBoardPost(id,updateBoardPostDto);
+
+		return ResponseEntity.ok(updatedBoardPostDto);
+	}
+
+	@PostMapping("/{postId}/comments")
+	public ResponseEntity<CommentDto> createComment(@PathVariable("postId") Long postId, @RequestBody CommentDto createCommentDto){
+		CommentDto createdCommentDto = boardPostService.createComment(postId, createCommentDto);
+		return ResponseEntity.ok(createdCommentDto);
+	}
+
+	@DeleteMapping("/{postId}/comments/{commentId}")
+	public ResponseEntity<Void> deleteComment(@PathVariable("postId") Long postId, @PathVariable("commentId") Long commentId) {
+		boardPostService.deleteComment(postId, commentId);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/example/basic/day240717_restapi/BoardPostService.java
+++ src/main/java/com/example/basic/day240717_restapi/BoardPostService.java
@@ -1,12 +1,16 @@
 package com.example.basic.day240717_restapi;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class BoardPostService {
 	List<BoardPost> boardPosts = new ArrayList<>();
 	private Long nextPostId = 1L;
@@ -14,9 +18,12 @@ public class BoardPostService {
 
 	public BoardPostDto createBoardPost(BoardPostDto boardPostDto) {
 		BoardPost boardPost = convertToBoardPostEntity(boardPostDto);
+		boardPost.setId(nextPostId++);
+		boardPost.setCreatedAt(LocalDateTime.now());
+		boardPosts.add(boardPost);
+		return convertToBoardPostDto(boardPost);
 
 	}
-
 
 	private static BoardPost convertToBoardPostEntity(BoardPostDto boardPostDto) {
 		BoardPost boardPost = new BoardPost();
@@ -34,7 +41,15 @@ public class BoardPostService {
 		return boardPost;
 	}
 
-	private static BoardPostDto convertToBoardPostDto(BoardPost boardPost) {
+	private static Comment convertToCommentEntity(CommentDto commentDto) {
+		Comment comment = new Comment();
+		comment.setId(commentDto.getId());
+		comment.setContent(commentDto.getContent());
+		comment.setAuthor(commentDto.getAuthor());
+		return comment;
+	}
+
+	private BoardPostDto convertToBoardPostDto(BoardPost boardPost) {
 		BoardPostDto boardPostDto = new BoardPostDto();
 		boardPostDto.setId(boardPost.getId());
 		boardPostDto.setTitle(boardPost.getTitle());
@@ -46,11 +61,80 @@ public class BoardPostService {
 		if (boardPost.getComments() != null) {
 			boardPostDto.setComments(
 				boardPost.getComments().stream()
-					.map(this::convertToCommentDto)
+					.map(BoardPostService::convertToCommentDto)
 					.collect(Collectors.toList())
 			);
 		}
 		return boardPostDto;
 	}
 
+	private static CommentDto convertToCommentDto(Comment comment) {
+		CommentDto commentDto = new CommentDto();
+		commentDto.setId(comment.getId());
+		commentDto.setContent(comment.getContent());
+		commentDto.setAuthor(comment.getAuthor());
+		commentDto.setCreatedAt(comment.getCreatedAt());
+		return commentDto;
+	}
+
+	public List<BoardPostDto> getAllBoardPosts() {
+		return boardPosts.stream()
+			.map(this::convertToBoardPostDto)
+			.collect(Collectors.toList());
+	}
+
+	public BoardPostDto getBoardPostDtoById(Long id) {
+		log.info("getBoardPostDtoById 로그");
+		return boardPosts.stream()
+			.filter(post -> post.getId().equals(id))
+			.map(this::convertToBoardPostDto)
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("id에 해당하는 글을 찾을 수 없습니다."));
+	}
+
+	public void deleteBoardPost(Long id) {
+		BoardPost boardPost = findBoardPostById(id);
+		boardPosts.remove(boardPost);
+		log.info("삭제 성공하였습니다.");
+	}
+
+	// 찾는 역할
+	private BoardPost findBoardPostById(Long id) {
+		return boardPosts.stream()
+			.filter(post -> post.getId().equals(id))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("id에 해당하는 글을 찾을 수 없습니다."));
+	}
+
+	public BoardPostDto updateBoardPost(Long id, BoardPostDto updateBoardPostDto) {
+		BoardPost boardPost = findBoardPostById(id);
+		boardPost.setTitle(updateBoardPostDto.getTitle());
+		boardPost.setContent(updateBoardPostDto.getContent());
+		boardPost.setUpdatedAt(LocalDateTime.now());
+		return convertToBoardPostDto(boardPost);
+
+	}
+
+	public CommentDto createComment(Long postId, CommentDto createCommentDto) {
+		BoardPost boardPost = findBoardPostById(postId);
+		Comment comment = convertToCommentEntity(createCommentDto);
+		comment.setId(nextCommentId++);
+		comment.setCreatedAt(LocalDateTime.now());
+		boardPost.addComment(comment);
+		return convertToCommentDto(comment);
+	}
+
+	public void deleteComment(Long postId, Long commentId) {
+		BoardPost boardPost = findBoardPostById(postId);
+		Comment comment = findCommentById(commentId, boardPost);
+
+		boardPost.removeComment(comment);
+	}
+
+	private static Comment findCommentById(Long commentId, BoardPost boardPost) {
+		return boardPost.getComments().stream()
+			.filter(c -> c.getId().equals(commentId))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
+	}
 }
