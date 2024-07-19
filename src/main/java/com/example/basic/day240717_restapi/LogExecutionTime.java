9e92e524402e49b0f1e5052695add1eb8ff96187
HEO <cloudisme99@gmail.com>
2024-07-19 20:36:48 +0900

    day240719 add

---
+++ src/main/java/com/example/basic/day240717_restapi/LogExecutionTime.java
@@ -0,0 +1,11 @@
+package com.example.basic.day240717_restapi;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface LogExecutionTime {
+}
